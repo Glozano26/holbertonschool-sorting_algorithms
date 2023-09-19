b598bcf4f7e73e5751e895d6793f1907a4be22eb
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-19 10:50:56 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,35 @@
 #include "sort.h"
-
+/**
+  *cambiar_pos - intercambia el valor
+  *@n1: pointer n1
+  *@n2: pointer n2
+  */
 void cambiar_pos(int *n1, int *n2)
 {
 	int temp = *n1;
 	*n1 = *n2;
 	*n2 = temp;
 }
-
+/**
+  *bubble_sort - function that sorts an array of integers in ascending
+  *@size: size array
+  *Return: void
+  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size -1; i++)
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
 
-		for (j = 0; j < size-i-1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
-				cambiar_pos(&array[j],&array[j+1]);
+				cambiar_pos(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
 		}
