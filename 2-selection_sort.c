0b723a2390ad570cfd93086f2c258a96aab60fb4
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-20 13:34:19 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,22 @@
 
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j - 1;
+	unsigned int i = 0, j =  1;
 	int least, aux, least_index;
 
 	while (i < size - 1)
 	{
-		least - array[1];
+		least = array[i];
 		while (j < size)
 		{
 			if (least > array[j])
 			{
 				least_index = j;
-				least = array[i];
+				least = array[j];
 			}
 			j++;
 		}
-		if (i != least_index)
+		if (i != (unsigned int)least_index)
 		{
 			aux = array[i];
 			array[i] = least;
