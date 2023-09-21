1cf30a5d8179bc89333e0231e308be3d238b2a1a
Glozano26 <lozsuperbia2014@gmail.com>
2023-09-21 12:54:14 -0700

    quicksort

--- sort.h
+++ sort.h
@@ -2,7 +2,7 @@
 #define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -23,5 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quicksort_low_high(int *array, int low_idx, int high_idx, size_t size);
+int Lotumo_partition(int *array, int low_idx, int high_idx, size_t size);
 
 #endif
